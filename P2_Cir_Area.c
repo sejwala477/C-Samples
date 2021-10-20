a4cd6259fb1a19bdbdbdceb05d8af28be9f06939
sejwala477 <92861970+sejwala477@users.noreply.github.com>
2021-10-20 17:25:53 +0530

    Update P2_Cir_Area.c

--- P2_Cir_Area.c
+++ P2_Cir_Area.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    float radius;
-    printf("Enter Radius(cm) : ");
-    scanf("%f",&radius);
-    printf("Area of circle is : %f", 3.14 * radius * radius);
+    float radius_of_circle;
+    printf("Enter Radius(in cm) : ");
+    scanf("%f",&radius_of_circle);
+    printf("Area of circle is : %f", 3.14 * radius_of_circle * radius_of_circle);
 }
